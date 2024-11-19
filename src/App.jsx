f585b18983a8f00afc42cc222e83709330373800
risyadkamarullahsolecode1 <risyad.kamarullah@solecode.id>
2024-11-19 13:06:23 +0700

    update the search, paginate, filter, and sotring for book and add member component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './App.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/templates/Header';
 import HomePage from './components/pages/HomePage';
 import BooksPage from './components/pages/BooksPage';
+import MembersPage from './components/organisms/MemberList';
+import AddMemberPage from './components/pages/AddMemberPage';
+import AddBookPage from './components/pages/AddBookPage';
+import EditBookPage from './components/pages/EditBookPage';
+import EditMemberPage from './components/pages/EditMemberPage';
+import BookDetailPage from './components/pages/BookDetailPage';
+import MemberDetailPage from './components/pages/MemberDetailPage';
 
 export const queryClient = new QueryClient();
 
@@ -16,9 +23,15 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/books" element={<BooksPage />} />
+        <Route path="/books:id" element={<BookDetailPage />} />
+        <Route path="/books/new" element={<AddBookPage />} />
+        <Route path="/books/edit/:id" element={<EditBookPage />} />
+        <Route path="/members" element={<MembersPage />} />
+        <Route path="/members/new" element={<AddMemberPage />} />
+        <Route path="/members/:id" element={<MemberDetailPage />} />
+        <Route path="/members/edit/:id" element={<EditMemberPage />} />
       </Routes>
     </Router>
-    
     </QueryClientProvider>
   )
 }
